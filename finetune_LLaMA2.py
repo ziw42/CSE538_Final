3dddb609037ccc56e7e363c7e00d032e0ec426c8
jackpurcell <ljczmh@gmail.com>
2024-05-06 10:50:25 -0400

    new

--- finetune_LLaMA2.py
+++ finetune_LLaMA2.py
@@ -2,51 +2,26 @@ from transformers import AdamW
 from utils.utils import loadModel, MRMDataset, int2str, createLoaderMRM
 import torch
 from tqdm import tqdm
+from finetune import finetune
 
-### Set hyperparameters
-batch_size = 4
-lr = 1e-5
-num_epochs = 1
-max_length = 256
+if __name__ == "__main__":
+    ### Set hyperparameters
+    batch_size = 4
+    lr = 1e-5
+    num_epochs = 1
+    max_length = 256
+    dataset_name = "mrm8488/fake-news"
 
-### Load model
-tokenizer, model = loadModel("meta-llama/Llama-2-7b-chat-hf")
-
-### Load data
-loader = createLoaderMRM(tokenizer, batch_size, split="train", max_length=256)
-
-### Set optimizer
-optimizer = AdamW(model.parameters(), lr=lr)
-
-model.train()
-print("######Start training######")
-
-### Training loop
-for epoch in range(num_epochs):  
-    for batch in tqdm(loader):
-        outputs = model(input_ids=batch['input_ids'], attention_mask=batch['attention_mask'], labels=batch['labels'])
-        loss = outputs.loss
-        loss.backward()
-        optimizer.step()
-        optimizer.zero_grad()
-        print(f"Epoch {epoch}, Loss: {loss.item()}")
-
-### Evaluate the model
-model.eval()
-predictions = []
-loader = createLoaderMRM(tokenizer, batch_size, split="test")
-for batch in loader:
-    outputs = model(input_ids=batch['input_ids'], attention_mask=batch['attention_mask'])
-    logits = outputs.logits
-    predictions += torch.argmax(logits, dim=-1).tolist()
-
-
-
-
-### Save the finetuned model
-model.save_pretrained('/home/jkl6486/fknews')
-print()
-
+    finetune("meta-llama/Llama-2-7b-chat-hf", 
+             dataset_name, 
+             batch_size=batch_size, 
+             learning_rate=lr, 
+             num_epochs=num_epochs, 
+             max_length=max_length, 
+             vocab=False, 
+             train=True, 
+             evaluation=True, 
+             save_step=100)
 
 
 
